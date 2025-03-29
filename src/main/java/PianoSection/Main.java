8c77155223555d5c5ebeaf27deb1a2fde9fc6c53
Alex <alexander.little01@gmail.com>
2025-03-28 21:13:25 -0400

    Piano fxml location correction
    Corrected the reference to the fxml location for the piano

--- src/main/java/PianoSection/Main.java
+++ src/main/java/PianoSection/Main.java
@@ -1,3 +1,5 @@
+package PianoSection;
+
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
@@ -17,7 +19,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/Resources/Notes.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/other_fxmls/Notes.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();

751324820348cae36c8b7d0f890b20afcfbe6859
Ahmet Yusuf Yildirim <140295077+AyYildirim-05@users.noreply.github.com>
2025-04-06 22:43:00 -0400

    finalized the audio filters

---
+++ src/main/java/org/JStudio/Plugins/audioFilterMain.java
@@ -0,0 +1,39 @@
+package org.JStudio.Plugins;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+import static org.JStudio.Plugins.audioFilters.*;
+
+public class audioFilterMain {
+    public static void main(String[] args) throws LineUnavailableException, UnsupportedAudioFileException, IOException {
+        String inputFile = "src/main/resources/test_inputs_music.wav";
+        String outputFile = "filtered_output.wav";
+
+        // Read WAV file
+        File file = new File(inputFile);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        AudioFormat format = audioStream.getFormat();
+        byte[] audioBytes = audioStream.readAllBytes();
+        audioStream.close();
+
+        // Convert byte[] to short[]
+        short[] samples = audioFilters.bytesToShorts(audioBytes);
+
+        // Apply your own low-pass filter
+        float sampleRate = format.getSampleRate();
+        float cutoffFrequency = 1000f; // in Hz
+        applyHighPassFilter(samples, sampleRate, cutoffFrequency);
+
+        // Convert short[] back to byte[]
+        byte[] filteredBytes = shortsToBytes(samples);
+
+        // Save filtered audio to new WAV file
+        saveWavFile(outputFile, filteredBytes, format);
+        System.out.println("Filtered audio saved as: " + outputFile);
+
+        // Play filtered sound
+        playAudio(filteredBytes, format);
+    }
+}
--- src/main/java/org/JStudio/Plugins/audioFilters.java
+++ src/main/java/org/JStudio/Plugins/audioFilters.java
@@ -1,12 +1,17 @@
 package org.JStudio.Plugins;
 
 
+import javax.sound.sampled.*;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+
 public class audioFilters {
 
     /*
     removes high frequencies
      */
-    private static void applyLowPassFilter(short[] samples, float sampleRate, float cutoffFreq) {
+    public static void applyLowPassFilter(short[] samples, float sampleRate, float cutoffFreq) {
         double RC = 1.0 / (2 * Math.PI * cutoffFreq);
         double dt = 1.0 / sampleRate;
         double alpha = dt / (RC + dt);
@@ -20,7 +25,7 @@ public class audioFilters {
     /*
     removes low frequencies
      */
-    private static void applyHighPassFilter(short[] samples, float sampleRate, float cutoffFreq) {
+    public static void applyHighPassFilter(short[] samples, float sampleRate, float cutoffFreq) {
         double RC = 1.0 / (2 * Math.PI * cutoffFreq);
         double dt = 1.0 / sampleRate;
         double alpha = RC / (RC + dt);
@@ -34,4 +39,39 @@ public class audioFilters {
         }
     }
 
+    public static short[] bytesToShorts(byte[] audioBytes) {
+        short[] samples = new short[audioBytes.length / 2];
+        for (int i = 0; i < samples.length; i++) {
+            samples[i] = (short) ((audioBytes[2 * i + 1] << 8) | (audioBytes[2 * i] & 0xFF));
+        }
+        return samples;
+    }
+
+    // Convert short array back to 16-bit PCM byte array
+    public static byte[] shortsToBytes(short[] samples) {
+        byte[] audioBytes = new byte[samples.length * 2];
+        for (int i = 0; i < samples.length; i++) {
+            audioBytes[2 * i] = (byte) (samples[i] & 0xFF);
+            audioBytes[2 * i + 1] = (byte) ((samples[i] >> 8) & 0xFF);
+        }
+        return audioBytes;
+    }
+
+    // Save byte[] to WAV file
+    public static void saveWavFile(String filename, byte[] audioBytes, AudioFormat format) throws IOException {
+        File file = new File(filename);
+        ByteArrayInputStream bais = new ByteArrayInputStream(audioBytes);
+        AudioInputStream audioStream = new AudioInputStream(bais, format, audioBytes.length / format.getFrameSize());
+        AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, file);
+    }
+
+    // Play audio from byte[]
+    public static void playAudio(byte[] audioBytes, AudioFormat format) throws LineUnavailableException {
+        SourceDataLine line = AudioSystem.getSourceDataLine(format);
+        line.open(format);
+        line.start();
+        line.write(audioBytes, 0, audioBytes.length);
+        line.drain();
+        line.close();
+    }
 }

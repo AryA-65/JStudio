34d86cee9c9ebe33e3751467de3a9e37b8687da6
TheodoreGeorgiou <6247319@edu.vaniercollege.qc.ca>
2025-04-18 15:10:20 -0400

    Exception handling for csv writing/reading

--- src/main/java/login/UserDataController.java
+++ src/main/java/login/UserDataController.java
@@ -15,6 +15,8 @@ import java.util.HashMap;
 public class UserDataController {
     public HashMap<String, User> users = new HashMap<>();
     private final String filePath = "src/main/resources/UserData/loginInfo.csv";
+    private String finalLine;
+    private boolean emptyFinalLine;
 
     /**
      * Creates a file used for storing all user login data
@@ -23,14 +25,6 @@ public class UserDataController {
         File csvFile = new File(filePath);
         try {
             csvFile.createNewFile();
-            PrintWriter printwriter;
-            try {
-                printwriter = new PrintWriter(new FileOutputStream(filePath, true), true);
-                printwriter.println("admin," + "password," + "1," + "1");
-                printwriter.close();
-            } catch (FileNotFoundException e) {
-                System.out.println(e);
-            }
         } catch (IOException ex) {
             System.out.println("Error.");
         }
@@ -40,14 +34,35 @@ public class UserDataController {
      * Reads the content of the .csv file containing all user login data
      */
     public void readFile() {
+        boolean emptyFirstLine = true;
         String line;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            
+            // Add all users in the hashmap until there are none left
             while ((line = reader.readLine()) != null) {
+                finalLine = line;
+                emptyFirstLine = false;
                 String[] fields = line.split(",");
-                // Create a user to be stored in the list of existing users
-                User user = new User(fields[0], fields[1], Integer.parseInt(fields[2]), Integer.parseInt(fields[3]));
-                users.put(fields[0], user);
+                
+                // Check for empty lines/wrong format lines and skip if there are
+                if (fields.length == 4) {
+                    // Create a user to be stored in the list of existing users
+                    User user = new User(fields[0], fields[1], Integer.parseInt(fields[2]), Integer.parseInt(fields[3]));
+                    users.put(fields[0], user);
+                }
+            }
+            
+            // If no lines are present, add the admin account
+            if (emptyFirstLine) {
+                PrintWriter printwriter;
+                try {
+                    printwriter = new PrintWriter(new FileOutputStream(filePath, true), true);
+                    printwriter.println("admin," + "password," + "1," + "1");
+                    printwriter.close();
+                } catch (FileNotFoundException e) {
+                    System.out.println(e);
+                }
             }
         } catch (IOException e) {
             System.out.println(e);
@@ -61,21 +76,39 @@ public class UserDataController {
     public void writeToFile(User user) {
         users.clear();
         readFile();
-        for (String username : users.keySet()) {
-            if (!users.containsKey(user.getUserName())) {
-                PrintWriter printwriter;
-                try {
-                    printwriter = new PrintWriter(new FileOutputStream(filePath, true), true);
-                    printwriter.println(user.toString());
-                    printwriter.close();
-                    return;
-                } catch (FileNotFoundException e) {
-                    System.out.println(e);
-                }
+        if (!users.containsKey(user.getUserName())) {
+            PrintWriter printwriter;
+            try {
+                checkFinalLine();
+                printwriter = new PrintWriter(new FileOutputStream(filePath, true), true);
+                printwriter.println(user.toString());
+                printwriter.close();
+                return;
+            } catch (FileNotFoundException e) {
+                System.out.println(e);
             }
         }
     }
 
+    /**
+     * Checks if there is an empty line after each user and adds one to avoid
+     * the new user being added on the same line as the previous one
+     */
+    private void checkFinalLine() {
+        PrintWriter printwriter;
+        try {
+            printwriter = new PrintWriter(new FileOutputStream(filePath, true), true);
+            // If theres no empty line at the end, add one
+            if (!finalLine.isEmpty() && finalLine!=null) {
+                printwriter.println();
+            }
+            printwriter.close();
+            return;
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+    
     /**
      * Checks whether a username exists in the users HashMap
      * @param username the username to search for
@@ -84,7 +117,7 @@ public class UserDataController {
     public boolean isUserInFile(String username) {
         return users.containsKey(username);
     }
-
+    
     /**
      * Retrieves the password for a given username from the users HashMap
      * @param username the username to search for
@@ -98,7 +131,4 @@ public class UserDataController {
     public HashMap<String, User> getUsers() {
         return users;
     }
-
-
 }
-

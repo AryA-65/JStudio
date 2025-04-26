b219e9f6d237ee789f80bdb9bdfcddf563f68d8d
Alex <alexander.little01@gmail.com>
2025-04-26 11:17:50 -0400

    The Synth Piano can now be opened in the plugins menu

---
+++ src/main/java/PianoManualSynth/SynthPiano.java
@@ -0,0 +1,30 @@
+package PianoManualSynth;
+
+import PianoSection.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class SynthPiano{
+
+    public void openSynthPiano() {
+        try {
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            Parent root = FXMLLoader.load(ClassLoader.getSystemResource("other_fxmls/Notes_1.fxml"));
+            Scene scene = new Scene(root,1500,900);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+}

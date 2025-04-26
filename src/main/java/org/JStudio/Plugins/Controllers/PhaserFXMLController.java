78cc895049675444cdd86bb152d59391de9c742e
TheodoreGeorgiou <6247319@edu.vaniercollege.qc.ca>
2025-04-25 21:00:08 -0400

    Added knobs to phaser

--- src/main/java/org/JStudio/Plugins/Controllers/PhaserFXMLController.java
+++ src/main/java/org/JStudio/Plugins/Controllers/PhaserFXMLController.java
@@ -4,7 +4,10 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.layout.GridPane;
 import org.JStudio.Plugins.Models.PhaserPlugin;
+import org.JStudio.UI.Knob;
+import static org.JStudio.UI.Knob.Type.REG;
 
 /**
  * FXML controller class for the Phaser UI
@@ -21,6 +24,11 @@ public class PhaserFXMLController {
     private Button resetButton;
     @FXML
     private Button playButton;
+    @FXML
+    private GridPane grid;
+    Knob frequencyKnob = new Knob(100, false, 0, REG);
+    Knob deviationKnob = new Knob(100, true, 0.1, REG);
+    Knob wetDryKnob = new Knob(100, false, 0, REG);
     private PhaserPlugin phaser;
     
     /**
@@ -31,45 +39,71 @@ public class PhaserFXMLController {
     public void initialize() {
         phaser = new PhaserPlugin(100000, 8, 0.5); // Create a phaser
         
-        frequencySlider.setMin(1);
-        frequencySlider.setMax(10);
-        frequencySlider.setShowTickMarks(true);
-        frequencySlider.setMajorTickUnit(1);
-        frequencySlider.setMinorTickCount(0);
-        frequencySlider.setShowTickLabels(true);
-        frequencySlider.setValue(2);
+        frequencyKnob.setTranslateX(52);
+        deviationKnob.setTranslateX(52);
+        wetDryKnob.setTranslateX(52);
         
-        deviationSlider.setMin(5);
-        deviationSlider.setMax(10);
-        deviationSlider.setShowTickMarks(true);
-        deviationSlider.setMajorTickUnit(1);
-        deviationSlider.setMinorTickCount(0);
-        deviationSlider.setShowTickLabels(true);
-        deviationSlider.setSnapToTicks(true);
-        deviationSlider.setValue(2);
-        
-        wetDrySlider.setMin(1);
-        wetDrySlider.setMax(10);
-        wetDrySlider.setShowTickMarks(true);
-        wetDrySlider.setMajorTickUnit(1);
-        wetDrySlider.setMinorTickCount(0);
-        wetDrySlider.setShowTickLabels(true);
-        wetDrySlider.setValue(5);
-        
-        // Set listeners and actions for sliders and buttons
-        frequencySlider.valueProperty().addListener((ObservableValue<? extends Number> frequency, Number oldFrequency, Number newFrequency) -> {
-            phaser.setFrequency(newFrequency.intValue()*50000);
+        grid.add(frequencyKnob, 0, 1);
+        grid.add(deviationKnob, 1, 1);
+        grid.add(wetDryKnob, 2, 1);
 
+        frequencyKnob.valueProperty().addListener((ObservableValue<? extends Number> frequency, Number oldFrequency, Number newFrequency) -> {
+            newFrequency = frequencyKnob.getValue() * 10 * 50000;
+            System.out.println(newFrequency);
+            phaser.setFrequency(newFrequency.intValue());
         });
         
-        deviationSlider.valueProperty().addListener((ObservableValue<? extends Number> deviation, Number oldDeviation, Number newDeviation) -> {
+        deviationKnob.valueProperty().addListener((ObservableValue<? extends Number> deviation, Number oldDeviation, Number newDeviation) -> {
+            newDeviation = deviationKnob.getValue() * 10;
+            System.out.println(newDeviation);
             phaser.setDeviation(newDeviation.intValue());
-            
         });
         
-        wetDrySlider.valueProperty().addListener((ObservableValue<? extends Number> decayTime, Number oldWetDryFactor, Number newWetDryFactor) -> {
-            phaser.setWetDryFactor(newWetDryFactor.doubleValue()/10);
+        wetDryKnob.valueProperty().addListener((ObservableValue<? extends Number> decayTime, Number oldWetDryFactor, Number newWetDryFactor) -> {
+            newWetDryFactor = wetDryKnob.getValue();
+            System.out.println(newWetDryFactor);
+            phaser.setWetDryFactor(newWetDryFactor.doubleValue());
         });
+        
+//        frequencySlider.setMin(1);
+//        frequencySlider.setMax(10);
+//        frequencySlider.setShowTickMarks(true);
+//        frequencySlider.setMajorTickUnit(1);
+//        frequencySlider.setMinorTickCount(0);
+//        frequencySlider.setShowTickLabels(true);
+//        frequencySlider.setValue(2);
+//        
+//        deviationSlider.setMin(5);
+//        deviationSlider.setMax(10);
+//        deviationSlider.setShowTickMarks(true);
+//        deviationSlider.setMajorTickUnit(1);
+//        deviationSlider.setMinorTickCount(0);
+//        deviationSlider.setShowTickLabels(true);
+//        deviationSlider.setSnapToTicks(true);
+//        deviationSlider.setValue(2);
+//        
+//        wetDrySlider.setMin(1);
+//        wetDrySlider.setMax(10);
+//        wetDrySlider.setShowTickMarks(true);
+//        wetDrySlider.setMajorTickUnit(1);
+//        wetDrySlider.setMinorTickCount(0);
+//        wetDrySlider.setShowTickLabels(true);
+//        wetDrySlider.setValue(5);
+//        
+//        // Set listeners and actions for sliders and buttons
+//        frequencySlider.valueProperty().addListener((ObservableValue<? extends Number> frequency, Number oldFrequency, Number newFrequency) -> {
+//            phaser.setFrequency(newFrequency.intValue()*50000);
+//
+//        });
+//        
+//        deviationSlider.valueProperty().addListener((ObservableValue<? extends Number> deviation, Number oldDeviation, Number newDeviation) -> {
+//            phaser.setDeviation(newDeviation.intValue());
+//            
+//        });
+//        
+//        wetDrySlider.valueProperty().addListener((ObservableValue<? extends Number> decayTime, Number oldWetDryFactor, Number newWetDryFactor) -> {
+//            phaser.setWetDryFactor(newWetDryFactor.doubleValue()/10);
+//        });
         
         // Play the audio
         playButton.setOnAction(e -> {

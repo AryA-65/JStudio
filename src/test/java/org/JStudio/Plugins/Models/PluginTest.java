1774613d8612212b6ba0d73a7238edcea2224e85
Alex <alexander.little01@gmail.com>
2025-05-05 14:40:24 -0400

    Fixed Byte Reading Testing

--- src/test/java/org/JStudio/Plugins/Models/PluginTest.java
+++ src/test/java/org/JStudio/Plugins/Models/PluginTest.java
@@ -1,5 +1,6 @@
 package org.JStudio.Plugins.Models;
 
+import java.io.File;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,27 +12,27 @@ public class PluginTest {
      */
     @Test
     public void testConvertFileToByteTestingMethod() {
-        String filePathName1 = "\\src\\main\\resources\\JUnitAudioTestFiles\\cowbell-74767.wav";
-        String filePathName2 = "\\src\\main\\resources\\JUnitAudioTestFiles\\dirty-tony-cowbell-mx-013-107592.wav";
-        String filePathName3 = "\\src\\main\\resources\\JUnitAudioTestFiles\\lp-cowbell-83904.wav";
-        String filePathName4 = "\\src\\main\\resources\\JUnitAudioTestFiles\\phonk-cowbell-1-200542.wav";
-        String filePathName5 = "\\src\\main\\resources\\JUnitAudioTestFiles\\synth-cowbell-high-169970.wav";
-        String filePathName6 = "\\src\\main\\resources\\JUnitAudioTestFiles\\synth-cowbell-low-169971.wav";
-        String filePathName7 = "\\src\\main\\resources\\JUnitAudioTestFiles\\underwater-white-noise-46423.wav";
-        String filePathName8 = "\\src\\main\\resources\\JUnitAudioTestFiles\\white-noise-179828.wav";
-        String filePathName9 = "\\src\\main\\resources\\JUnitAudioTestFiles\\white-noise-sweep-up-85276.wav";
-        String filePathName10 = "\\src\\main\\resources\\JUnitAudioTestFiles\\woosh-13225.wav";
+        String filePathName1 = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "JUnitAudioTestFiles" + File.separator + "cowbell-74767.wav";
+        String filePathName2 = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "JUnitAudioTestFiles" + File.separator + "dirty-tony-cowbell-mx-013-107592.wav";
+        String filePathName3 = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "JUnitAudioTestFiles" + File.separator + "lp-cowbell-83904.wav";
+        String filePathName4 = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "JUnitAudioTestFiles" + File.separator + "phonk-cowbell-1-200542.wav";
+        String filePathName5 = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "JUnitAudioTestFiles" + File.separator + "synth-cowbell-high-169970.wav";
+        String filePathName6 = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "JUnitAudioTestFiles" + File.separator + "synth-cowbell-low-169971.wav";
+        String filePathName7 = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "JUnitAudioTestFiles" + File.separator + "underwater-white-noise-46423.wav";
+        String filePathName8 = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "JUnitAudioTestFiles" + File.separator + "white-noise-179828.wav";
+        String filePathName9 = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "JUnitAudioTestFiles" + File.separator + "white-noise-sweep-up-85276.wav";
+        String filePathName10 = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "JUnitAudioTestFiles" + File.separator + "woosh-13225.wav";
         
         PluginTesting plugin = new PluginTesting();
-        assertEquals(plugin.convertFileToByteTestingMethod(filePathName1),plugin.convertFileToByteTestingMethod(filePathName1));
-        assertEquals(plugin.convertFileToByteTestingMethod(filePathName2),plugin.convertFileToByteTestingMethod(filePathName2));
-        assertEquals(plugin.convertFileToByteTestingMethod(filePathName3),plugin.convertFileToByteTestingMethod(filePathName3));
-        assertEquals(plugin.convertFileToByteTestingMethod(filePathName4),plugin.convertFileToByteTestingMethod(filePathName4));
-        assertEquals(plugin.convertFileToByteTestingMethod(filePathName5),plugin.convertFileToByteTestingMethod(filePathName5));
-        assertEquals(plugin.convertFileToByteTestingMethod(filePathName6),plugin.convertFileToByteTestingMethod(filePathName6));
-        assertEquals(plugin.convertFileToByteTestingMethod(filePathName7),plugin.convertFileToByteTestingMethod(filePathName7));
-        assertEquals(plugin.convertFileToByteTestingMethod(filePathName8),plugin.convertFileToByteTestingMethod(filePathName8));
-        assertEquals(plugin.convertFileToByteTestingMethod(filePathName9),plugin.convertFileToByteTestingMethod(filePathName9));
-        assertEquals(plugin.convertFileToByteTestingMethod(filePathName10),plugin.convertFileToByteTestingMethod(filePathName10));
+        assertArrayEquals(plugin.convertFileToByteTestingMethod(filePathName1),plugin.convertFileToByteTestingMethod(filePathName1));
+        assertArrayEquals(plugin.convertFileToByteTestingMethod(filePathName2),plugin.convertFileToByteTestingMethod(filePathName2));
+        assertArrayEquals(plugin.convertFileToByteTestingMethod(filePathName3),plugin.convertFileToByteTestingMethod(filePathName3));
+        assertArrayEquals(plugin.convertFileToByteTestingMethod(filePathName4),plugin.convertFileToByteTestingMethod(filePathName4));
+        assertArrayEquals(plugin.convertFileToByteTestingMethod(filePathName5),plugin.convertFileToByteTestingMethod(filePathName5));
+        assertArrayEquals(plugin.convertFileToByteTestingMethod(filePathName6),plugin.convertFileToByteTestingMethod(filePathName6));
+        assertArrayEquals(plugin.convertFileToByteTestingMethod(filePathName7),plugin.convertFileToByteTestingMethod(filePathName7));
+        assertArrayEquals(plugin.convertFileToByteTestingMethod(filePathName8),plugin.convertFileToByteTestingMethod(filePathName8));
+        assertArrayEquals(plugin.convertFileToByteTestingMethod(filePathName9),plugin.convertFileToByteTestingMethod(filePathName9));
+        assertArrayEquals(plugin.convertFileToByteTestingMethod(filePathName10),plugin.convertFileToByteTestingMethod(filePathName10));
     }
 }

32c317408bafc3eb5e73a1883254a11841ed1909
Ahmet Yusuf Yildirim <140295077+AyYildirim-05@users.noreply.github.com>
2025-05-05 21:02:39 -0400

    documentation

--- src/main/java/org/JStudio/Utils/ExporterImporter.java
+++ src/main/java/org/JStudio/Utils/ExporterImporter.java
@@ -7,7 +7,20 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * Class that export and imports a song
+ */
 public class ExporterImporter { //move export audio and file loader to this class
+    /**
+     * Exports a Song object to the user's local file system.
+     * The file will be saved in the directory: {/Music/JStudio/saved_songs/}
+     * with a ".song" extension.
+     * If the directory doesn't exist, it will be created.
+     *
+     * @param song   The object to export. Must implement
+     * @param f_name The file name (without an extension) under which the song will be saved
+     * @throws IOException If an I/O error occurs during writing the file
+     */
     public static void exportSong(Song song, String f_name) throws IOException {
         Path musicDir = Paths.get(System.getProperty("user.home"), "Music", "JStudio", "saved_songs");
         if (!musicDir.toFile().exists()) Files.createDirectories(musicDir);
@@ -19,6 +32,16 @@ public class ExporterImporter { //move export audio and file loader to this clas
         }
     }
 
+    /**
+     * Imports a Song object from a file stored on the local file system.
+     * The file is expected to be located in: {/Music/JStudio/saved_songs/}
+     * and have a ".song" extension.
+     *
+     * @param f_name The name of the file (without extension) to import the song from.
+     * @return The deserialized Song object.
+     * @throws IOException            If an I/O error occurs during file access
+     * @throws ClassNotFoundException If the class definition of the serialized object is not found
+     */
     public static Song importSong(String f_name) throws IOException, ClassNotFoundException {
         Path song_f = Paths.get(System.getProperty("user.home"), "Music", "JStudio", "saved_songs", f_name + ".song");
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(song_f.toFile()))) {
--- src/main/java/org/JStudio/Utils/FFTHandler.java
+++ src/main/java/org/JStudio/Utils/FFTHandler.java
@@ -1,26 +1,38 @@
 package org.JStudio.Utils;
 
 import org.jtransforms.fft.DoubleFFT_1D;
-
 import java.util.ArrayList;
 
+/**
+ * Class that handles FFT operations.
+ */
 public class FFTHandler {
    private double[] fftData;
    private int fftSize = 1024;
    private DoubleFFT_1D fft;
 
+    /**
+     * Creates a new FFT instance with default size (1024)
+     */
    public FFTHandler() {
        fftData = new double[fftSize * 2];
        fft = new DoubleFFT_1D(fftSize);
-       fftData = new double[fftSize * 2];
    }
 
+    /**
+     * Creates a new FFT instance with specified size
+     * @param size Size of the FFT window
+     */
    public FFTHandler(int size) {
        fftSize = size;
        fftData = new double[fftSize * 2];
        fft = new DoubleFFT_1D(fftSize);
    }
 
+    /**
+     * Processes FFT on a list of float samples
+     * @param inputData List of float samples
+     */
    public void processFFT(ArrayList<Float> inputData) {
        for (int i = 0; i < fftSize * 2; i++) {
            fftData[i * 2] = inputData.get(i);
@@ -29,7 +41,10 @@ public class FFTHandler {
 
        fft.realForward(fftData);
    }
-
+    /**
+     * Processes FFT on a list of short samples
+     * @param inputData List of short samples
+     */
    public void processFFT(short[] inputData) {
        for (int i = 0; i < fftSize * 2; i++) {
            fftData[i * 2] = inputData[i];
@@ -39,6 +54,10 @@ public class FFTHandler {
        fft.realForward(fftData);
    }
 
+    /**
+     * Returns the computed FFT data.
+     * @return The FFT output array.
+     */
    public double[] getFFTData() {
        return fftData;
    }
